7991b6696615203a1bb08de6b2880acc42fc66f4
Ubuntu <ubuntu@ip-172-31-39-238.ec2.internal>
2024-04-02 19:29:03 +0000

    file format has been changed

--- data_script.sh
+++ data_script.sh
@@ -1,7 +1,14 @@
-# Example Python script
-data = {
-    "name": "RaghavendarRavi",
-    "value": 42
-}
+#!/bin/bash
 
-print(data)
+# Sample data
+data="This is some sample test email data"
+
+# Print the data
+echo "Here's the data:"
+echo "$data"
+
+# Additional commands (optional)
+# You can add commands to process or manipulate the data here
+# For example, to get the current date and time:
+# echo "Current date and time:"
+# date
